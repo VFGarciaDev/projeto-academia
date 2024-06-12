14071dbaf4ecbac23e936d501c96627bfc4a2b40
Victor Feitosa Garcia <vfeitosa.garcia@gmail.com>
2024-06-12 12:08:06 -0300

    11 criar componente planos (#15)
    * feat: ignore vercel
    
    * #
    
    * feat: add component Planos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import '@/styles/App.css'
-import { Header } from './components/header/header'
 import { ThemeProvider } from './components/theme-provider/theme-provider'
+import { Header } from './components/header/header'
+import { Planos } from './components/planos/planos'
 
 function App() {
-
 
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Header />
-
+      <Planos />
     </ThemeProvider>
   )
 }
