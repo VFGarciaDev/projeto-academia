3ad9d9bdc043e46676958d597c55517c86c8153f
Caio Sverzut Padovan <caiopadovan1@hotmail.com>
2024-06-14 19:34:42 -0300

    feat: add Footer Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import '@/styles/App.css'
 import { ThemeProvider } from './components/theme-provider/theme-provider'
 import { Header } from './components/header/header'
 import { Planos } from './components/planos/planos'
+import { Footer } from './components/footer/footer'
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      
       <Header />
       <Planos />
+      <Footer />
     </ThemeProvider>
   )
 }
